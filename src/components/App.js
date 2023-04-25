369c2dfcd6089c1ccff71d979dfd968466277a19
nyamvularinga <nyamvularinga@gmail.com>
2023-04-25 09:37:08 +0300

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+export default function App() {
+  const [dogImg, setDogImg] = useState();
+
+  const fetchData = async () => {
+   try {
+     const res = await fetch("https://dog.ceo/api/breeds/image/random");
+     const data = await res.json();
+     setDogImg(data.message);
+   } catch (error) {
+     console.error("Error fetching data:", error);
+   }
+ }
+
+  useEffect(() => {
+   fetchData();
+  }, []);
+  return (
+    <>
+      {dogImg ? (<img src={dogImg} alt="Random dog" />) : (<p>Loading...</p>)}
+    </>
+  );
+}
